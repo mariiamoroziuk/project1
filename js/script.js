031f520d4418a295b8b6631ddbe9795058a17e9b
Igor Tkachenko <tigorlab@gmail.com>
2019-09-12 15:37:23 +0300

    small fix

--- js/script.js
+++ js/script.js
@@ -99,49 +99,17 @@ function onReady() {
             let newX = clientX - shiftX;
             let newY = clientY - shiftY;
 
-            // check if the new coordinates are below the bottom window edge
-            let newBottom = newY + dragElement.offsetHeight; // new bottom
+            let limits = {
+                top: container.offsetTop,
+                right: container.offsetWidth + container.offsetLeft - dragElement.offsetWidth ,
+                bottom: container.offsetHeight + container.offsetTop - dragElement.offsetHeight ,
+                left: container.offsetLeft
+            };
 
-            // below the window? let's scroll the page
-            if (newBottom > document.documentElement.clientHeight) {
-                // window-relative coordinate of document end
-                let docBottom = document.documentElement.getBoundingClientRect().bottom;
-
-                // scroll the document down by 10px has a problem
-                // it can scroll beyond the end of the document
-                // Math.min(how much left to the end, 10)
-                let scrollY = Math.min(docBottom - newBottom, 1);
-
-                // calculations are imprecise, there may be rounding errors that lead to scrolling up
-                // that should be impossible, fix that here
-                if (scrollY < 0) scrollY = 0;
-
-                window.scrollBy(0, scrollY);
-
-                // a swift mouse move make put the cursor beyond the document end
-                // if that happens -
-                // limit the new Y by the maximally possible (right at the bottom of the document)
-                newY = Math.min(newY, document.documentElement.clientHeight - dragElement.offsetHeight);
-            }
-
-            // check if the new coordinates are above the top window edge (similar logic)
-            if (newY < 0) {
-                // scroll up
-                let scrollY = Math.min(-newY, 1);
-                if (scrollY < 0) scrollY = 0; // check precision errors
-
-                window.scrollBy(0, -scrollY);
-                // a swift mouse move can put the cursor beyond the document start
-                newY = Math.max(newY, 0); // newY may not be below 0
-            }
-
-
-            // limit the new X within the window boundaries
-            // there's no scroll here so it's simple
-            if (newX < 0) newX = 0;
-            if (newX > document.documentElement.clientWidth - dragElement.offsetWidth) {
-                newX = document.documentElement.clientWidth - dragElement.offsetWidth;
-            }
+            if (newX > limits.right) newX = limits.right;
+            if (newX < limits.left) newX = limits.left;
+            if (newY > limits.bottom) newY = limits.bottom;
+            if (newY < limits.top) newY = limits.top;
 
             dragElement.style.left = newX + 'px';
             dragElement.style.top = newY + 'px';
